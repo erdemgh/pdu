8e6dc707d3b4ee1e6a8595cb2e097f30d719c392
qa ywg <erdem.cirik2@gmail.com>
2023-10-23 17:29:06 +0300

    initial

---
+++ flaski2.py
@@ -0,0 +1,34 @@
+import serial
+import time
+from flask import Flask, render_template, request
+
+ser = serial.Serial('/dev/ttyUSB0', 9600)
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index2.html')  # index2.html kullan
+
+@app.route('/submit', methods=['GET'])  # GET isteği için güncelleme
+def submit():
+    command = request.args.get('command')  # GET ile gelen 'command' parametresini al
+
+    if command.lower() == 'q':
+        return "Program sonlandırıldı."
+    elif command == 'c':
+        ser.write(command.encode())
+        time.sleep(0.30)
+        data = ser.readline()
+        return data.decode()
+    elif command in ['0', '1']:
+        ser.write(command.encode())
+        time.sleep(0.30)
+        data = ser.readline()
+        return data.decode()
+    else:
+        return "Geçersiz komut. Sadece '0', '1', 'c' veya 'q' kabul edilir."
+
+if __name__ == '__main__':
+    #app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=2129)
