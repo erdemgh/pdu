1cb17e517151be923b4603c7fd8af8d65411a4fa
qa ywg <erdem.cirik2@gmail.com>
2023-10-26 15:27:03 +0300

    20231026_1425 - release

--- pdu_killer.py
+++ pdu_killer.py
@@ -1,30 +1,13 @@
-import platform
 import subprocess
-import time
 
 def stop_pdu_processes():
-  """Stops all running PDU processes."""
-
-  process_list = subprocess.check_output(['ps', 'aux']).decode().split('\n')
-
-  for line in process_list:
-    if "pdu_" in line and ".py" in line:
-      pid = line.split()[1]
-      process_name = line.split()[-1]
-
-      if platform.system() == "Windows":
-        subprocess.run(["taskkill", "/f", "/t", "/im", process_name])
-      else:
-        subprocess.run(["kill", "-15", pid])
-
-      try:
-        subprocess.run(["kill", pid], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-      except subprocess.CalledProcessError:
-        time.sleep(5)
-        subprocess.run(["kill", "-9", pid])
-        print(f"{process_name} (PID {pid}) süreci zorla sonlandırıldı.")
-
-      print(f"{process_name} (PID {pid}) süreci sonlandırıldı.")
+    process_list = subprocess.check_output(['ps', 'aux']).decode().split('\n')
+    for line in process_list:
+        if "pdu_" in line:
+            pid = line.split()[1]
+            process_name = line.split()[-1]
+            subprocess.run(['kill', '-9', pid])
+            print(f"{process_name} (PID {pid}) süreci sonlandırıldı.")
 
 if __name__ == "__main__":
-  stop_pdu_processes()
+    stop_pdu_processes()
